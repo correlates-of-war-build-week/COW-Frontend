5de61be1cba49703047370280759bdb864df3c33
Reed James <reedtjames1234@gmail.com>
2019-06-26 13:34:48 -0500

    Map has icons displaying, Working on pulling data from backend

--- cow/src/components/Map.js
+++ cow/src/components/Map.js
@@ -1,6 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactMapGL from "react-map-gl";
-import war from "../data/war.json";
+import { Marker, Popup } from "react-map-gl";
+import * as countries from "../data/war.json";
+
+// import 'font-awesome/css/font-awesome.min.css';
+// import tank from "../icon/tank.svg"
 
 // change into class
 // take out useState and change to state
@@ -9,6 +13,7 @@ import war from "../data/war.json";
 //CREATE AN AXIOSWITHOUTAUTH, remove const token, remove headers
 
 const Map = () => {
+  //   console.log("Hey look at me!", countries);
   const [viewport, setViewport] = useState({
     latitude: 45,
     longitude: 45,
@@ -16,6 +21,22 @@ const Map = () => {
     height: "100vh",
     zoom: 10
   });
+
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  useEffect(() => {
+    const warInfo = e => {
+      if (e.key === "Escape") {
+        setSelectedCountry(null);
+      }
+    };
+    window.addEventListener("click", warInfo);
+
+    return () => {
+      window.removeEventListener("click", warInfo);
+    };
+  }, []);
+
   return (
     <div>
       <ReactMapGL
@@ -28,8 +49,41 @@ const Map = () => {
         onViewportChange={viewport => {
           setViewport(viewport);
         }}
-      />
-      {/* {war.country.map} */}
+      >
+        {countries.default.map((country, i) => {
+          //   console.log("heres the lat!", country.latitude);
+          if (country.latitude || country.longitude) {
+            return (
+              <Marker
+                key={country.name}
+                longitude={country.longitude}
+                latitude={country.latitude}
+              >
+                <button className="marker-btn">
+                  <img className="img-btn" src="../icon/tank.svg" alt="tank" />
+                </button>
+              </Marker>
+            );
+          }
+          //   else {
+
+          //     console.log(`bad coordinates at ${i}`);
+          //   }
+        })}
+        {selectedCountry ? (
+          <Popup
+            latitude={selectedCountry.latitude}
+            longitude={selectedCountry.longitude}
+            onClose={() => {
+              selectedCountry(null);
+            }}
+          >
+            <div>
+              <h1>hello</h1>
+            </div>
+          </Popup>
+        ) : null}
+      </ReactMapGL>
     </div>
   );
 };
